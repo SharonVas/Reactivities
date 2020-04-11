3dc1cc72282ce183142cb2a332c65abd6c8af4b6
SharonVas <svnb2011@gmail.com>
2020-04-11 21:01:00 +0300

    Section 12 upto lesson 165

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,13 @@
 using API.Middleware;
 using Application.Activities;
+using Application.Interfaces;
+using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -41,6 +45,13 @@ namespace API
             {
                 configurationExpression.RegisterValidatorsFromAssemblyContaining<Create>();
             });
+            var builder = services.AddIdentityCore<AppUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType,builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext>();
+            identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+            services.AddAuthentication();
+
+            services.AddScoped<IJwtGenerator,JwtGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
