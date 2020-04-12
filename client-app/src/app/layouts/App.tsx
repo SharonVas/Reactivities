c817e119399d26bbd945755e857b4fc5f2e8632d
SharonVas <svnb2011@gmail.com>
2020-04-12 18:26:06 +0300

    Section 13 upto lesson 186

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -8,13 +8,30 @@ import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import NotFound from './NotFound';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { LoginForm } from '../../features/user/LoginForm';
+import { RootStoreContext } from '../stores/rootStore';
+import { LoadingComponents } from './LoadingComponents';
+import ModalContainer from '../common/modals/ModalContainer'
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+  const { getUser } = rootStore.userStore;
+
+  useEffect(() => {
+    if (token) {
+      getUser().finally(() => setAppLoaded());
+    }else{
+      setAppLoaded();
+    }
+  }, [getUser, setAppLoaded, token])
+
+  if(!appLoaded) return<LoadingComponents content='Loading app...'/>
 
   return (
     <Fragment>
+      <ModalContainer/>
       <ToastContainer position='top-center'/>
       <Route exact path='/' component={HomePage} />
       <Route path={'/(.+)'} render={() => (
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -4,12 +4,13 @@ import agent from "../api/agent";
 import { RootStore } from "./rootStore";
 import { history } from "../..";
 
+
 export default class UserStore {
     rootStore: RootStore;
-    constructor(rootStore:RootStore){
-        this.rootStore= rootStore;
+    constructor(rootStore: RootStore) {
+        this.rootStore = rootStore;
     }
-    
+
     @observable user: IUser | null = null;
 
     @computed get isLoggedIn() { return !!this.user }
@@ -17,14 +18,31 @@ export default class UserStore {
     @action login = async (values: IUserFormValues) => {
         try {
             const user = await agent.User.login(values);
-            runInAction(()=>{
+            runInAction(() => {
                 this.user = user;
             })
-            console.log(user);
+            this.rootStore.commonStore.setToken(user.token);
             history.push('/activities');
 
         } catch (error) {
             throw error;
         }
+    }
+
+    @action getUser = async () => {
+        try {
+            const user = await agent.User.current();
+            runInAction(() => {
+                this.user = user;
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    @action logout = () => {
+        this.rootStore.commonStore.setToken(null);
+        this.user = null;
+        history.push('/');
     }
 }
