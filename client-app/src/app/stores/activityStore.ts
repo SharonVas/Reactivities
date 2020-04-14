7253772c8e33ce499fc3380f0e79b22f7c3728e5
SharonVas <svnb2011@gmail.com>
2020-04-14 15:59:09 +0300

    section 13 remove comment

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -28,7 +28,6 @@ export default class ActivityStore {
         );
         return Object.entries(sortedActivities.reduce((activities, activity) => {
             const date = activity.date.toISOString().split('T')[0];
-            //const date = activity.date.toISOString().split('T')[0];
             activities[date] = activities[date] ? [...activities[date], activity] : [activity];
             return activities;
         }, {} as { [key: string]: IActivity[] }));
