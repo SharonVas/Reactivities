0c59eb27c715edc157add1d33d329d08cf4fe0e6
SharonVas <svnb2011@gmail.com>
2020-04-15 22:53:08 +0300

    End of Section 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -13,7 +15,7 @@ namespace Application.Activities
         public class Command : IRequest
         {
             public Guid Id { get; set; }
-            
+
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
@@ -26,37 +28,52 @@ namespace Application.Activities
         {
             public CommandValidator()
             {
-                RuleFor(x=>x.Title).NotEmpty();
-                RuleFor(x=>x.Description).NotEmpty();
-                RuleFor(x=>x.Category).NotEmpty();
-                RuleFor(x=>x.Date).NotEmpty();
-                RuleFor(x=>x.City).NotEmpty();
-                RuleFor(x=>x.Venue).NotEmpty();
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
             }
         }
 
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
-                _context= context;
+                _userAccessor = userAccessor;
+                _context = context;
             }
 
-            public async Task<Unit> Handle (Command request, CancellationToken cancellationToken)
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = new Activity
                 {
-                    Id= request.Id,
-                    Title= request.Title,
-                    Description=request.Description,
-                    Category=request.Category,
-                    Date=request.Date,
-                    City=request.City,
-                    Venue= request.Venue
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description,
+                    Category = request.Category,
+                    Date = request.Date,
+                    City = request.City,
+                    Venue = request.Venue
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserActivity{
+                    AppUser=user,
+                    Activity=activity,
+                    IsHost=true,
+                    DateJoined=DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendee);
+                
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
